de3951c2267a1e34364091caf5826d0bbf6daf8b
Yassir El yaqine <yel-yaqi@e3r8p12.1337.ma>
2024-09-01 19:12:36 +0100

    @ infrastructure for ex02 @

---
+++ cpp_00/ex02/Account.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/01 18:42:36 by yel-yaqi          #+#    #+#             */
+/*   Updated: 2024/09/01 18:51:11 by yel-yaqi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+void Account::makeDeposit(int deposit)
+{
+	(void)deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	return (withdrawal);
+}
+
+int Account::checkAmount() const
+{
+	return (0);
+}
+
+void Account::displayStatus() const
+{
+	
+}
+
+void Account::displayAccountsInfos()
+{
+	
+}
+
+Account::Account(int initial_deposit)
+{
+	(void)initial_deposit;
+}
+
+Account::~Account()
+{
+	
+}
