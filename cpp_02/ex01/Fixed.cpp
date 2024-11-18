8ef7058b0aca7c3d04455ef9d7ac8299bd6d1747
Yassir El yaqine <yel-yaqi@e3r7p16.1337.ma>
2024-11-18 17:40:05 +0100

    @ finished cpp02/ex01 @

--- cpp_02/ex01/Fixed.cpp
+++ cpp_02/ex01/Fixed.cpp
@@ -6,13 +6,35 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:09:15 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/11/14 17:40:20 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:38:41 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <iostream>
+#include <math.h>
 
 int const Fixed::frac = 8;
+
+double poww(double base, double exp)
+{
+	double pow = 1;
+	if (exp == 0)
+		return 1;
+	if (exp < 0)
+	{
+		exp *= -1;
+		base = 1 / base;
+	}
+	if (exp == 1)
+		return (base);
+	while (exp != 0)
+	{
+		pow *= base;
+		exp = exp - 1;
+	}
+	return pow;
+}
 
 Fixed::Fixed()
 {
@@ -52,4 +74,32 @@ void Fixed::setRawBits(int const raw)
 {
 	fixed = raw;
 	std::cout << "setRawBits member function called" << std::endl;
+}
+
+Fixed::Fixed(int const i)
+{
+	std::cout << "Int constructor called" << std::endl;
+	fixed = i << 8;
+}
+
+Fixed::Fixed(float const f)
+{
+	std::cout << "Float constructor called" << std::endl;
+	fixed = roundf(f * poww(2, 8));
+}
+
+float Fixed::toFloat() const
+{
+	return (fixed * poww(2, -8));
+}
+
+int Fixed::toInt() const
+{
+	return (fixed >> 8);
+}
+
+std::ostream& operator<< (std::ostream &out, Fixed const &fp)
+{
+	out << fp.toFloat();
+	return (out);
 }
--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:09:01 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/11/14 17:39:03 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/11/18 17:16:22 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,24 @@ class Fixed
 		int fixed;
 		static const int frac;
 	public:
+		// constructors
 		Fixed ();
-		Fixed (const Fixed &f);
-		Fixed& operator=(const Fixed &f);
-		~Fixed ();
+		Fixed (int const i);
+		Fixed (float const f);
+		Fixed (const Fixed &f);		
 
+		// overloads
+		Fixed& operator= (const Fixed &f);
+
+		// members
 		int getRawBits () const;
 		void setRawBits (int const raw);
+		float toFloat () const;
+		int toInt () const;
+
+		~Fixed ();
+
+		friend std::ostream& operator<< (std::ostream &out, Fixed const &fp);
+
 };
+
