405a00414d7bab2f6a75c7a4a6630e425ed8aeaa
Yassir El yaqine <yel-yaqi@e3r8p1.1337.ma>
2024-11-24 20:30:25 +0100

    @ finished cpp02/ex02 @

--- cpp_02/ex02/main.cpp
+++ cpp_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 17:09:03 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/11/24 20:10:17 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/11/24 20:29:34 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 
 int main( void ) 
 {
-	Fixed a = Fixed(0.00390625f);
-	Fixed  b( Fixed( 5.05f ) * Fixed( 2 ) );
-	std::cout << a << std::endl; // 0
-	std::cout << --a << std::endl; // 0.00390625
-	std::cout << a << std::endl; // 0.00390625
-	std::cout << a-- << std::endl; // 0.00390625
-	std::cout << a << std::endl; // 0.0078125
-	std::cout << b << std::endl; // 10.1016
-	// std::cout << Fixed::max( a, b * Fixed(-1) ) << std::endl; // 10.1016
-
-	// min
-	// / + -
-	return 0;
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
 }
