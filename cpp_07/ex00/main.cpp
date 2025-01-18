52057d28495e83b060008be20f1a683c88c7b05c
Yassir El yaqine <yel-yaqi@e3r2p7.1337.ma>
2025-01-18 13:18:10 +0100

    @ finished cpp07/ex00 @

---
+++ cpp_07/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 12:45:35 by yel-yaqi          #+#    #+#             */
+/*   Updated: 2025/01/18 13:03:55 by yel-yaqi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "template.hpp"
+#include <iostream>
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
