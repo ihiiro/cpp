cf777acb8f9a9a5bc87cb92e0bab4329073941c3
Yassir El yaqine <yel-yaqi@e3r1p10.1337.ma>
2025-02-11 22:53:14 +0100

    @ just backing up @

--- cpp_09/ex00/main.cpp
+++ cpp_09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 17:23:12 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2025/01/26 19:23:40 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2025/02/11 22:52:07 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 #include <fstream>
 
 #include <cstring>
+
+typedef std::ifstream::traits_type traits_type;
 
 int main(int argc, char **argv)
 {
@@ -40,22 +42,55 @@ int main(int argc, char **argv)
 
 
 	/* unopenable/empty file protection */
-	std::ifstream ifstream_input_file(filename); // input file
-	if (not ifstream_input_file)
+	std::ifstream input_file(filename); // input file
+	if (not input_file)
 		return fatal_error("couldn't open input file for reading"), 1;
-	if (ifstream_input_file.get() == std::ifstream::traits_type::eof())
+	if (input_file.get() == traits_type::eof())
 		return usage_error("empty input file"), 1;
-	std::ifstream ifstream_ref_file(ref_filename); // reference file
-	if (not ifstream_ref_file)
+	std::ifstream ref_file(ref_filename); // reference file
+	if (not ref_file)
 		return fatal_error("couldn't open reference file for reading"), 1;
-	if (ifstream_ref_file.get() == std::ifstream::traits_type::eof())
+	if (ref_file.get() == traits_type::eof())
 		return usage_error("empty reference file"), 1;
+	input_file.unget();
+	ref_file.unget();
 
 
 
-
+	/*    proto   */
 	
+	char c = input_file.get();
+
+
+
+	while (c != '\n' and c != traits_type::eof())
+	{
+		std::cout << c;
+
+		
+		
+		c = input_file.get();
+	}
+
+
+
+
+
 	
 	
 	
 }
+
+/*
+
+process_year( stream )
+	1st character is 2 then (add equivalent to yi) else (throw "BAD LINE")
+	2nd character is 0 then (add equivalent to yi) else (throw "BAD LINE")
+	3rd character in range [0, 2] then (add equivalent to yi) else (throw "BAD LINE")
+	4th character in range [0, 9] then (add equivalent to yi) else (throw "BAD LINE")
+	return yi
+
+process_md( stream )
+	1st character in range [0, 1] then (add equivalent to yi) else (throw "BAD LINE")
+
+*/
