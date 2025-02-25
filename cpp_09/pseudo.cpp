058f92656374a7d98652a6a7d0ca9352c8a8d8da
Yassir El yaqine <yel-yaqi@e3r7p16.1337.ma>
2025-02-25 05:28:13 +0000

    @ pseudo code for cpp09/ex02 @

---
+++ cpp_09/pseudo.cpp
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+swap( &a , &b )
+	if (*a > *b)
+
+
+
+main_chain_merge_sort( list , left , right , pair_size )
+	if (pair_size == 0)
+			swap(&list[0], &list[1], pair_size)
+			return
+	
+	mid = left + (right - left) / 2
+	main_chain_merge_sort(list, )
+	swap(&list[pair_size - 1], &list[2 * pair_size - 1])
+	
+
+merge_insert( list )
+	if (list.len() % 2 == 0)
+		main_chain_merge_sort( list , 0, list.len() - 1 ,  list.len() / 2 )
+	insert_sort( list )
+
+
+	
+	
+
+
+
+*/
+
+
+
