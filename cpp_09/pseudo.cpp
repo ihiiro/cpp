aff0955c151deb175a957328c1a90150da60034d
Yassir El yaqine <yel-yaqi@e3r7p16.1337.ma>
2025-02-25 05:31:58 +0000

    @ routine backup @

--- cpp_09/pseudo.cpp
+++ cpp_09/pseudo.cpp
@@ -14,9 +14,17 @@
 
 /*
 
+
+
+
+
+
+
+
+
 swap( &a , &b )
 	if (*a > *b)
-
+		
 
 
 main_chain_merge_sort( list , left , right , pair_size )
@@ -25,18 +33,27 @@ main_chain_merge_sort( list , left , right , pair_size )
 			return
 	
 	mid = left + (right - left) / 2
-	main_chain_merge_sort(list, )
-	swap(&list[pair_size - 1], &list[2 * pair_size - 1])
+	main_chain_merge_sort(list, left, mid)
+	main_chain_merge_sort(list, mid + 1, right)
+	swap(&list[pair_size - 1], &list[2 * pair_size - 1], pair_size)
 	
 
 merge_insert( list )
 	if (list.len() % 2 == 0)
 		main_chain_merge_sort( list , 0, list.len() - 1 ,  list.len() / 2 )
+	else
+		main_chain_merge_sort( list , 0, list.len() - 2 ,  list.len() / 2 )
 	insert_sort( list )
 
 
 	
 	
+
+
+
+
+
+
 
 
 
