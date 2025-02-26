f5e7cdc87f9a2f6cd4a9c0272059212b74264c64
Yassir El yaqine <yel-yaqi@e3r4p12.1337.ma>
2025-02-26 05:35:43 +0000

    @ routine backup @

--- cpp_09/pseudo.cpp
+++ cpp_09/pseudo.cpp
@@ -12,6 +12,64 @@
 
 
 
+
+
+
+
+
+
+/*
+
+
+
+
+
+insert ( container , S )
+	S_copy = S
+	k = 2
+
+	LOOP
+		b = compute_b ( k )
+		
+	
+
+merge_insertion ( container , S )
+	if (container.len() == 1)
+		S.push( container[0] )
+		return
+
+	container_of_largest_in_pairs = get_from( &container )
+	merge_insertion( &container_of_largest_in_pairs , S )
+	S.push_front( S[0].minor )
+	insert( &container , S )
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 
@@ -33,13 +91,13 @@ swap( container , a_idx , b_idx , pair_size )
 
 main_chain_merge_sort( container , left , right , pair_size )
 	if (pair_size == 0)
-			swap(&container[0], &container[1], pair_size)
+			swap(left, right, 1)
 			return
 	
 	mid = left + (right - left) / 2
 	main_chain_merge_sort(container, left, mid, pair_size / 2)
 	main_chain_merge_sort(container, mid + 1, right, pair_size / 2)
-	swap(container, [pair_size - 1, 2 * pair_size - 1, pair_size)
+	swap(container, left + pair_size - 1, left + 2 * pair_size - 1, pair_size)
 	
 
 merge_insert( container )
