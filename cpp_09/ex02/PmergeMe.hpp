68bfc92fc87c2284cb27eba8c4cd9201ea5ef4fd
Yassir El yaqine <yel-yaqi@e3r7p9.1337.ma>
2025-03-05 17:15:13 +0000

    @ routine backup @

--- cpp_09/ex02/PmergeMe.hpp
+++ cpp_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 02:42:25 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2025/03/05 13:45:17 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2025/03/05 17:09:28 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 typedef struct atom_s
 {
-	int integer;
 	size_t p;
+	int integer;
 	struct atom_s *pair_chain;
 }  ATOM;
 
--- cpp_09/ex02/main.cpp
+++ cpp_09/ex02/main.cpp
@@ -6,84 +6,53 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 02:41:11 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2025/03/05 15:55:26 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2025/03/05 17:14:58 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-// #include <vector>
 #include <iostream>
 
 #include <vector>
 
+#include <deque>
+
+/*
+
+	N <- 1,000,000
+	DEPTH <- ~21
+	RAM <- 12mb
+	ATOM <- 24B
+
+*/
+
 int main(int argc, char **argv)
 {
-	std::vector < ATOM > X;
 
-	X.push_back ( (ATOM){ 3 , 0 , NULL } );
-	X.push_back ( (ATOM){ 0 , 0 , NULL } );
-	X.push_back ( (ATOM){ 2 , 0 , NULL } );
-	X.push_back ( (ATOM){ 1 , 0 , NULL } );
-	X.push_back ( (ATOM){ 4 , 0 , NULL } );
-	X.push_back ( (ATOM){ 5 , 0 , NULL } );
-	X.push_back ( (ATOM){ 6 , 0 , NULL } );
-	X.push_back ( (ATOM){ 33 , 0 , NULL } );
-	X.push_back ( (ATOM){ 52 , 0 , NULL } );
-	X.push_back ( (ATOM){ 7 , 0 , NULL } );
-	X.push_back ( (ATOM){ 6 , 0 , NULL } );
-	X.push_back ( (ATOM){ 9 , 0 , NULL } );
-	X.push_back ( (ATOM){ 5 , 0 , NULL } );
-	X.push_back ( (ATOM){ 1234 , 0 , NULL } );
-	X.push_back ( (ATOM){ -1234 , 0 , NULL } );
-	X.push_back ( (ATOM){ 12 , 0 , NULL } );
-	X.push_back ( (ATOM){ 18 , 0 , NULL } );
-	X.push_back ( (ATOM){ 22 , 0 , NULL } );
-	X.push_back ( (ATOM){ 34 , 0 , NULL } );
-	X.push_back ( (ATOM){ 43 , 0 , NULL } );
-	X.push_back ( (ATOM){ 55 , 0 , NULL } );
-	X.push_back ( (ATOM){ 97 , 0 , NULL } );
-	X.push_back ( (ATOM){ 28 , 0 , NULL } );
-	X.push_back ( (ATOM){ 4 , 0 , NULL } );
-	X.push_back ( (ATOM){ 4 , 0 , NULL } );
-	X.push_back ( (ATOM){ 4 , 0 , NULL } );
-	X.push_back ( (ATOM){ 0 , 0 , NULL } );
-	X.push_back ( (ATOM){ 2 , 0 , NULL } );
-	X.push_back ( (ATOM){ 1 , 0 , NULL } );
-	X.push_back ( (ATOM){ -3247 , 0 , NULL } );
-	X.push_back ( (ATOM){ 5 , 0 , NULL } );
-	X.push_back ( (ATOM){ 6 , 0 , NULL } );
-	X.push_back ( (ATOM){ 3 , 0 , NULL } );
-	X.push_back ( (ATOM){ -2147483648 , 0 , NULL } );
-	X.push_back ( (ATOM){ 7 , 0 , NULL } );
-	X.push_back ( (ATOM){ 6 , 0 , NULL } );
-	X.push_back ( (ATOM){ 9 , 0 , NULL } );
-	X.push_back ( (ATOM){ 5 , 0 , NULL } );
-	X.push_back ( (ATOM){ 1234 , 0 , NULL } );
-	X.push_back ( (ATOM){ -134 , 0 , NULL } );
-	X.push_back ( (ATOM){ 12 , 0 , NULL } );
-	X.push_back ( (ATOM){ 4 , 0 , NULL } );
-	X.push_back ( (ATOM){ 434234343 , 0 , NULL } );
-	X.push_back ( (ATOM){ 12 , 0 , NULL } );
-	X.push_back ( (ATOM){ 43 , 0 , NULL } );
-	X.push_back ( (ATOM){ 55 , 0 , NULL } );
-	X.push_back ( (ATOM){ 97 , 0 , NULL } );
-	X.push_back ( (ATOM){ 2147483647 , 0 , NULL } );
-	X.push_back ( (ATOM){ 4 , 0 , NULL } );
+	std::vector < ATOM > X_vector, S_vector;
+	std::deque < ATOM > X_deque, S_deque;
 
-	std::vector < ATOM > S;
+	if ( argc < 3 )
+	{
+
+		std::cerr << "ERROR: usage ./PmergeMe A B ..." << std::endl;
+		return 1;
+ 
+	}
+
+	if (argv == NULL)
+	{
+		
+		std::cerr << "ERROR: FATAL: NULL ARGV" << std::endl;
+		return 1;
+		
+	}
 
 	
-	merge_insertion < std::vector<ATOM> > ( X , S , 1 );
 
-	// std::cout << S [ 4 ].pair_chain << "\n" << std::endl;
-	// std::cout << &S [ 2 ] << "\n" << std::endl;
-
-
-	for ( size_t i = 0 ; i < S.size() ; i++ )
-		std::cout << S [ i ].integer << std::endl;
-
-	argc++ ; (void)argv;
+	
+	// merge_insertion < std::vector<ATOM> > ( X_vector , S_ , 1 );
 
 	return 0;
 	
